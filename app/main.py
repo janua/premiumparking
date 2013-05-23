7f6e39ada77bdc937cbebf9fc9e271ac0dd940cb
Francis Carr <francis.carr@guardian.co.uk>
2013-05-23 11:56:32 +0100

    Add all files

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template, jsonify
+app = Flask(__name__)
+
+STATE = dict()
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/state")
+def state():
+	return jsonify(STATE)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
